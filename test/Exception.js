af9231013276f429f16d031e12cb30f4d8f6e6b9
cryo <bhamon.cryo@gmail.com>
2015-09-12 15:24:21 +0200

    Test for Exception corrected.

--- test/Exception.js
+++ test/Exception.js
@@ -13,22 +13,21 @@ describe('Exception', function() {
 	describe('#constructor()', function() {
 		it('should be correctly default initialized', function() {
 			var ex = new lib.odin.Exception();
-
-			ex.should.have.an.enumerable('name').String.which.equal('Exception');
-			ex.should.have.an.enumerable('stack').String.which.is.not.empty;
-			ex.should.have.an.enumerable('message').String.which.equal('');
-			ex.should.have.an.enumerable('details').Object;
-			ex.should.have.an.enumerable('cause');
+			ex.should.have.a.property('name').which.is.an.instanceof(String).and.equal('Exception');
+			ex.should.have.a.property('stack').which.is.an.instanceof(String).and.is.not.empty;
+			ex.should.have.a.property('message').which.is.an.instanceof(String).and.equal('');
+			ex.should.have.a.property('details').which.is.an.instanceof(Object);
+			ex.should.have.a.property('cause');
 		});
 
 		it('should be correctly initialized', function() {
 			var ex = new lib.odin.Exception('message', {foo:'bar'}, new Error());
 
-			ex.should.have.an.enumerable('name').String.which.equal('Exception');
-			ex.should.have.an.enumerable('stack').String.which.is.not.empty;
-			ex.should.have.an.enumerable('message').String.which.equal('message');
-			ex.should.have.an.enumerable('details').which.eql({foo:'bar'});
-			ex.should.have.an.enumerable('cause').which.is.instanceof(Error);
+			ex.should.have.a.property('name').which.is.an.instanceof(String).and.equal('Exception');
+			ex.should.have.a.property('stack').which.is.an.instanceof(String).and.is.not.empty;
+			ex.should.have.a.property('message').which.is.an.instanceof(String).and.equal('message');
+			ex.should.have.a.property('details').which.is.an.instanceof(Object).and.deepEqual({foo:'bar'});
+			ex.should.have.a.property('cause').which.is.an.instanceof(Error);
 		});
 	});
 });
