e924da04360b9130848d078c6209bb7e9ee67b74
cryo <bhamon.cryo@gmail.com>
2015-09-12 20:33:22 +0200

    Test for util corrected.

--- test/util.js
+++ test/util.js
@@ -3,8 +3,7 @@
 var lib = {
 	deps:{
 		joi:require('joi'),
-		should:require('should'),
-		shouldPromised:require('should-promised')
+		should:require('should')
 	},
 	odin:{
 		util:require('../lib/util'),
@@ -42,52 +41,6 @@ describe('util', function() {
 			(function() {
 				lib.odin.util.validate('test', lib.deps.joi.string().uppercase().required()).should.be.a.String.which.is.equal('TEST');
 			}).should.not.throw();
-		});
-	});
-
-	describe('array', function() {
-		describe('.toSet()', function() {
-			it('should throw on bad parameters', function() {
-				(function() {
-					lib.odin.util.array.toSet();
-				}).should.throw(lib.odin.Exception);
-
-				(function() {
-					lib.odin.util.array.toSet(null);
-				}).should.throw(lib.odin.Exception);
-
-				(function() {
-					lib.odin.util.array.toSet(21);
-				}).should.throw(lib.odin.Exception);
-
-				(function() {
-					lib.odin.util.array.toSet('test');
-				}).should.throw(lib.odin.Exception);
-
-				(function() {
-					lib.odin.util.array.toSet({foo:'bar'});
-				}).should.throw(lib.odin.Exception);
-
-				(function() {
-					lib.odin.util.array.toSet(function() {});
-				}).should.throw(lib.odin.Exception);
-
-				(function() {
-					lib.odin.util.array.toSet([21, {foo:'bar'}]);
-				}).should.throw(lib.odin.Exception);
-			});
-
-			it('should return an empty set for an empty array', function() {
-				lib.odin.util.array.toSet([]).should.be.an.Object.which.is.empty;
-			});
-
-			it('should return a correctly filled object', function() {
-				lib.odin.util.array.toSet(['test', 'foo', 'bar']).should.be.an.Object.which.eql({
-					test:true,
-					foo:true,
-					bar:true
-				});
-			});
 		});
 	});
 });
